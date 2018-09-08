dccdd21f4dcae14d5c8ed413d17bce857a142e91
Dinu Stefan <dinustef@gmail.com>
2018-09-08 18:59:14 +0200

    Repos1

---
+++ eShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyShop.Core.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository
+    {
+
+    }
+
+    
+}
